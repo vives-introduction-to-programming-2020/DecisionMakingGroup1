675fed739da0a29f05ed3257883556d5f10022a8
Nico De Witte <nico.dewitte@vives.be>
2020-10-27 07:40:36 +0100

    Add code

---
+++ DecisionMaking/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        public static int GenerateSecret()
+        {
+            Random generator = new Random();
+            return generator.Next(1, 5);
+        }
+
+        public static int RequestUserGuess()
+        {
+            Console.WriteLine("Please enter a guess [1,4]: ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            return guess;
+        }
+
+        public static void EvaluateUserGuess(int secret, int userGuess)
+        {
+            if (secret == userGuess)
+            {
+                Console.WriteLine("Congratz. You guessed it right");
+            }
+            else if (secret < userGuess)
+            {
+                Console.WriteLine("Sorry you missed it.");
+                Console.WriteLine("You guessed to high");
+                Console.WriteLine("The secret was " + secret);
+            }
+            else
+            {
+                Console.WriteLine("Sorry you missed it.");
+                Console.WriteLine("You guessed to low");
+                Console.WriteLine("The secret was " + secret);
+            }
+
+            //else
+            //{
+            //    Console.WriteLine("Sorry you missed it.");
+            //    if (secret < userGuess)
+            //    {
+            //        Console.WriteLine("You guessed to high");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("You guessed to low");
+            //    }
+            //    Console.WriteLine("The secret was " + secret);
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to number guesser");
+
+            int secret = GenerateSecret();
+
+            int userGuess = RequestUserGuess();
+
+            EvaluateUserGuess(secret, userGuess);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Decision making
+
+User number number guessing game using if-else construct
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/DecisionMakingGroup1.git
+```
